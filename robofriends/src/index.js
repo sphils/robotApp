fb686db0a431a94568d6b8e046ebb58b624e2e6b
Shawn Phillips <shawnphillips@shawns-mac-mini.attlocal.net>
2018-09-08 07:58:15 -0500

    a

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,1 +1,16 @@
-// JavaScript Document
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Card from './Card';
+import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
+
+ReactDOM.render(
+                <div>
+                    <Card />
+                    <Card />
+                    <Card />
+                </div>
+                
+, document.getElementById('root'));
+registerServiceWorker();
